fa612f5997fda09c5fc472b9e8b240bc73cce6fc
Matt Ammirati <matt.ammirati@gmail.com>
2020-05-23 11:13:26 -1000

    added student email that checks if registered

--- ReactWeek1Assignment.js
+++ ReactWeek1Assignment.js
@@ -1,16 +1,29 @@
 class Student {
-    constructor (name, email, community) {
-        this.name = name;
-        this.email = email;
-        this.community = community;
-    }
+  constructor(name, email, community) {
+    this.name = name;
+    this.email = email;
+    this.community = community;
+  }
 }
 
 class Bootcamp {
-    constructor (name, level, students = []) {
-        this.name = name;
-        this.level = level;
-        this.students = students;
+  constructor(name, level, students = []) {
+    this.name = name;
+    this.level = level;
+    this.students = students;
+  }
 
+  registerStudent(student) {
+    const studentAlreadyRegistered = this.students.filter(
+      (s) => s.email === student.email
+    );
+
+    if (studentAlreadyRegistered) {
+      console.log(`The student with ${student.email} is already registered`);
     }
+
+    this.students.push(student);
+    console.log(`${student.email} is registered to ${this.name}`);
+    return this.students;
+  }
 }
