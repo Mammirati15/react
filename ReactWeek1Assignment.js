a13233acbdc14a37ee17393c5bca98d85dfcb2b9
Matt Ammirati <matt.ammirati@gmail.com>
2020-05-23 10:57:44 -1000

    added starter files, created student/bootcamp classes

---
+++ ReactWeek1Assignment.js
@@ -0,0 +1,16 @@
+class Student {
+    constructor (name, email, community) {
+        this.name = name;
+        this.email = email;
+        this.community = community;
+    }
+}
+
+class Bootcamp {
+    constructor (name, level, students = []) {
+        this.name = name;
+        this.level = level;
+        this.students = students;
+
+    }
+}
